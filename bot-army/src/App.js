4b26fda2f05fb858b8cdc8521393fbd789f236ad
Clare <clare.oparo@student.moringaschool.com>
2024-02-09 18:30:14 +0300

    setup project structure

---
+++ bot-army/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bot Army</h1>
+      {/* BotCollection and YourBotArmy components will go here */}
+    </div>
+  );
+}
+
+export default App;
